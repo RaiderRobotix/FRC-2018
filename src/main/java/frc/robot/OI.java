0ba2f2c3489f5d97734efcd7756d7dd103142496
hybras <24651269+hybras@users.noreply.github.com>
2020-02-16 13:42:37 -0500

    Remove button listeners and factory functions

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -19,19 +19,7 @@ final class OI {
 	static final OI m_instance = new OI();
 
 	void teleop() {
-
-		// =========== DRIVES ===========
-		if (getLeftTrigger()) {
-			m_drives.brakes();
-		} else if (getLeftButton(2)) {
-			m_drives.brakes(false);
-		}
-
-		if (!m_drives.brakes()) {
-			m_drives.speed(getLeftY(), getRightY());
-		} else {
-			m_drives.speed(0.0);
-		}
+		m_drives.speed(getLeftY(), getRightY());
 	}
 
 	double getLeftY() {
@@ -47,29 +35,5 @@ final class OI {
 	double getOperatorY() {
 		double yval = -m_operatorStick.getY();
 		return Math.abs(yval) < Constants.JOYSTICK_DEADBAND ? 0.0 : yval;
-	}
-
-	boolean getOperatorTrigger() {
-		return m_operatorStick.getTrigger();
-	}
-
-	boolean getOperatorButton(int btn) {
-		return m_operatorStick.getRawButton(btn);
-	}
-
-	boolean getRightTrigger() {
-		return m_rightStick.getTrigger();
-	}
-
-	boolean getLeftTrigger() {
-		return m_leftStick.getTrigger();
-	}
-
-	boolean getRightButton(int btn) {
-		return m_rightStick.getRawButton(btn);
-	}
-
-	boolean getLeftButton(int btn) {
-		return m_leftStick.getRawButton(btn);
 	}
 }
