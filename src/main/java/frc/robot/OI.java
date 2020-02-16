74beeb975bf03e8238ad052c8c3d94591e0cfc52
hybras <24651269+hybras@users.noreply.github.com>
2020-02-16 13:52:08 -0500

    Remove hardware classes from oi

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -4,10 +4,6 @@ import edu.wpi.first.wpilibj.Joystick;
 
 final class OI {
 
-	// ===== Robot Mechanisms =====
-	private final Drivebase m_drives = Drivebase.m_instance;
-	// private final Climber m_climber;
-
 	// ===== Joysticks =====
 	private final Joystick m_leftStick = new Joystick(Constants.LEFT_JOYSTICK_PORT);
 	private final Joystick m_rightStick = new Joystick(Constants.RIGHT_JOYSTICK_PORT);
